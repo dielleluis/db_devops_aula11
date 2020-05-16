f9e96445828affb7bb4b3a033fd10008911f09ce
Luis Dielle <luisdielle@Luiss-MacBook-Pro.local>
2020-05-15 23:08:49 -0300

    New setup

--- setup_database.py
+++ setup_database.py
@@ -4,10 +4,24 @@ conn = sqlite3.connect('example.db')
 
 c = conn.cursor()
 
-c.execute('''CREATE TABLE stocks
-                (date text, trans text, symbol text, qty real, price real)''')
+c.execute('''CREATE TABLE IF NOT EXISTS albums(
+                albumId integer PRIMARY KEY,
+                title text NOT NULL,
+                artistId integer NOT NULL,
+                FOREIGN KEY (artistId) REFERENCES artist)''')
 
-c.execute("INSERT INTO stocks VALUES ('2006-01-05', 'BUY', 'RHTAT', 100,35.14)")
+c.execute('''CREATE TABLE IF NOT EXISTS artist(
+                artistId integer PRIMARY KEY,
+                name text NOT NULL)''')
+
+c.execute(" INSERT INTO albums (albumId, title, artistId) VALUES(1, 'THE WALL', 100)")
+c.execute(" INSERT INTO albums (albumId, title, artistId) VALUES(2, 'BAND OF GYPSYS', 200)")
+c.execute(" INSERT INTO albums (albumId, title, artistId) VALUES(3, 'HOTEL CALIFORNIA', 300)")
+c.execute(" INSERT INTO artist (artistId, name) VALUES(100, 'PINKY FLOYD')")
+c.execute(" INSERT INTO artist (artistId, name) VALUES(200, 'JIMI HENDRIX')")
+c.execute(" INSERT INTO artist (artistId, name) VALUES(300, 'EAGLES')")
+
+c.execute("SELECT Title, Name FROM albums INNER JOIN artist ON artist.artistId = albums.artistId")
 
 conn.commit()
 conn.close()
